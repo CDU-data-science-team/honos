e8801ed93bd64d93ff54accf4d83bcf31ace0849
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-02-24 14:45:14 +0000

    add rename variables function and integrate into lag function

--- R/calculations.R
+++ R/calculations.R
@@ -5,30 +5,46 @@
 #' @param date_var Name of date (or datetime) variable
 #' @param add_change_label Logical, specifying whether to add a variable that describes the change using the function \code{\link{calc_change_label}}
 #' @param change_label String, specifying whether to describe the change using "high_low" (e.g., HL stands for high to low) or "deterio_improve" (Deterioration, Unchanged, Improved)
+#' @param value_vars_current Vector, specifying variable names with values for 'current' items
+#' @param prob_var_item8 Vector, specifying variable name with description of problem (prob) for item 8
+#' @param spec_var_item8 Vector, specifying variable name with problem specification (spec) of for item 8
+#' @param value_vars_history Vector, specifying specifying variable names with values for 'historic' items
+#' @param honos_version Vector, specifying version of the HoNOS that is being used. TODO IMPLEMENT MORE VERSIONS.
 #'
 #' @return Dataframe with original data and lagged values
 #' @export
 #'
 #' @examples
-#' lag_honos(data = honos_wide,
+#' lag_honos(data = honos_data,
 #'           id_var = id,
 #'           date_var = date,
-#'           change_label = "deterio_improve")
-lag_honos <- function(data, id_var, date_var, add_change_label = TRUE, change_label = c("high_low", "deterio_improve")) {
+#'           value_vars_current = c("q1", "q2", "q3", "q4", "q5", "q6", "q7",
+#'                                  "q8", "q9", "q10", "q11", "q12", "q13"),
+#'           prob_var_item8 = c("q8_prob"),
+#'           spec_var_item8 = c("q8_spec"),
+#'           value_vars_history = c("qa", "qb", "qc", "qd", "qe"))
+lag_honos <- function(data, id_var, date_var, value_vars_current, prob_var_item8, spec_var_item8, value_vars_history, add_change_label = TRUE, change_label = c("high_low", "deterio_improve"), honos_version = c("working_adults")) {
 
   # Check arguments
   change_label <- match.arg(change_label)
 
   # TODO ADD SOME DATA CHECKS
+  # Rename data
+  honos_renamed <- rename_honos(data = data,
+                                value_vars_current = value_vars_current,
+                                prob_var_item8 = prob_var_item8,
+                                spec_var_item8 = spec_var_item8,
+                                value_vars_history = value_vars_history,
+                                honos_version = honos_version)
 
   # TODO THIS NEEDS TO BE IMPROVED
   # MAKE THIS MORE GENERIC SO IT WORKS WITH ALL SORTS OF VARIABLE NAMES
   # Create vectors of variable names
-  honos_scales_new_names <- paste0("honos_i", 1:13)
-  honos_scales_lag_names <- paste0("lag1", "honos", "_", "i", 1:13)
+  honos_scales_new_names <- paste0("honos_i", 1:18, "_value")
+  honos_scales_lag_names <- paste0("lag1", "honos", "_", "i", 1:18, "_value")
 
   # Create lag1 in wide format
-  data_lag <- data %>%
+  data_lag <- honos_renamed %>%
     dplyr::group_by({{ id_var }}) %>%
     dplyr::arrange({{ id_var }}, {{ date_var }}) %>%
     dplyr::mutate(dplyr::across(.cols = c({{ date_var }}, {{ honos_scales_new_names }}),
@@ -41,24 +57,24 @@ lag_honos <- function(data, id_var, date_var, add_change_label = TRUE, change_la
   data_change <- data_lag %>%
     tidyr::pivot_longer(cols = dplyr::all_of(c({{ honos_scales_new_names }}, {{ honos_scales_lag_names }})),
                         values_to = "value",
-                        names_to = c("lag", "item"),
+                        names_to = c("lag", "item", "type"),
                         names_sep = "_") %>%
-    dplyr::mutate(item = factor(stringr::str_extract(item, "\\d+"),
-                                labels = 1:13),
+    dplyr::mutate(item = as.numeric(stringr::str_extract(item, "\\d+")),
                   value = as.numeric(value)) %>%
     tidyr::pivot_wider(names_from = lag,
-                       values_from = value) %>%
-    dplyr::mutate(honos_change = lag1honos - honos,
-                  honos_date_diff := {{ date_var }} - !! rlang::sym(paste0("lag1", rlang::ensym(date_var)))) %>%
+                       values_from = value,
+                       names_glue = "{lag}_value") %>%
+    dplyr::mutate(honos_change = lag1honos_value - honos_value,
+                  honos_date_diff := lubridate::as.difftime({{ date_var }} - !! rlang::sym(paste0("lag1", rlang::ensym(date_var))))) %>%
     tidyr::drop_na(honos_date_diff)
 
   # Add change label
   if (add_change_label == TRUE) {
 
     data_change <- data_change %>%
-      dplyr::mutate(honos_change_label = calc_change_label(value = honos,
-                                                         lag_value = lag1honos,
-                                                         change_label = change_label))
+      dplyr::mutate(honos_change_label = calc_change_label(value = honos_value,
+                                                           lag_value = lag1honos_value,
+                                                           change_label = change_label))
 
   }
 
@@ -141,7 +157,7 @@ calc_subscales <- function(data, id_var, date_var, item_var, value_var, return_f
                                                     {{ item_var }} %in% c(4:5) ~ "impairment",
                                                     {{ item_var }} %in% c(6:8) ~ "symptom",
                                                     {{ item_var }} %in% c(9:12) ~ "social")) %>%
-    dplyr::group_by(id, date, honos_subscale) %>%
+    dplyr::group_by({{ id_var }}, {{ date_var }}, honos_subscale) %>%
     dplyr::mutate(honos_subscale_value = sum({{ value_var }})) %>%
     dplyr::ungroup()
 
@@ -160,8 +176,6 @@ calc_subscales <- function(data, id_var, date_var, item_var, value_var, return_f
       return(data_temp)
 
     }
-
-
 
   } else if (return_format == "wide") {
 
