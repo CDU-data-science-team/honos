50d49c5a62d821b8262d553b1d2f53147205599b
Zoë Turner <zoe.turner2@nottshc.nhs.uk>
2022-03-02 09:36:40 +0000

    Added NEWS and refined tests for rename()

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
-3# honos 0.1.2
+# honos 0.2.0
+
+Work in progress on tests - issue may be related to other package {nottshcData}
+
+Removed argument for different versions of Honos so defaults to working adults.
+
+# honos 0.1.2
+honos 0.1.2
 
 * Added a `NEWS.md` file to track changes to the package.
--- tests/testthat/test-rename.R
+++ tests/testthat/test-rename.R
@@ -11,12 +11,16 @@ testthat::test_that("Check data is returned by row count for a SQL connection", 
     honos::rename_honos(
       data = data,
       value_vars_current = c(
-        "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8Score",
-        "Q9", "Q10", "Q11", "Q12", "Q13"
+        "Q1", "Q2", "Q3",
+        "Q4", "Q5", "Q6",
+        "Q7", "Q8Score",
+        "Q9", "Q10", "Q11",
+        "Q12", "Q13"
       ),
       prob_var_item8 = "Q8Problem",
       spec_var_item8 = "Q8JSpec",
-      value_vars_history = c("QA", "QB", "QC", "QD", "QE")) %>%
+      value_vars_history = c("QA", "QB", "QC",
+                             "QD", "QE")) %>%
     dplyr::count() %>%
     dplyr::collect()
 
@@ -35,12 +39,16 @@ testthat::test_that("Check data is returned by row count for a data frame", {
     honos::rename_honos(
       data = data,
       value_vars_current = c(
-        "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8Score",
-        "Q9", "Q10", "Q11", "Q12", "Q13"
+        "Q1", "Q2", "Q3",
+        "Q4", "Q5", "Q6",
+        "Q7", "Q8Score",
+        "Q9", "Q10", "Q11",
+        "Q12", "Q13"
       ),
       prob_var_item8 = "Q8Problem",
       spec_var_item8 = "Q8JSpec",
-      value_vars_history = c("QA", "QB", "QC", "QD", "QE")) %>%
+      value_vars_history = c("QA", "QB", "QC",
+                             "QD", "QE")) %>%
     dplyr::count() %>%
     dplyr::collect()
 
@@ -67,12 +75,16 @@ testthat::test_that("Check for column names expected", {
     honos::rename_honos(
       data = data,
       value_vars_current = c(
-        "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8Score",
-        "Q9", "Q10", "Q11", "Q12", "Q13"
+        "Q1", "Q2", "Q3",
+        "Q4", "Q5", "Q6",
+        "Q7", "Q8Score",
+        "Q9", "Q10", "Q11",
+        "Q12", "Q13"
       ),
       prob_var_item8 = "Q8Problem",
       spec_var_item8 = "Q8JSpec",
-      value_vars_history = c("QA", "QB", "QC", "QD", "QE")) %>%
+      value_vars_history = c("QA", "QB", "QC",
+                             "QD", "QE")) %>%
     dplyr::select(7, 8, 15, 16, 17, 26
     ) %>%
     dplyr::collect()
@@ -83,8 +95,6 @@ testthat::test_that("Check for column names expected", {
   )
 })
 
-
-# Shouldn't work as the value_vars_current are SQL names not tidy
 testthat::test_that("Check for column names expected", {
   col_names <- c("honos_i1_value",
                  "honos_i2_value",
@@ -101,12 +111,15 @@ testthat::test_that("Check for column names expected", {
     honos::rename_honos(
       data = data,
       value_vars_current = c(
-        "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8Score",
-        "Q9", "Q10", "Q11", "Q12", "Q13"
+        "q1", "q2", "q3",
+        "q4", "q5", "q6",
+        "q7", "q8score", "q9",
+        "q10", "q11", "q12", "q13"
       ),
-      prob_var_item8 = "Q8Problem",
-      spec_var_item8 = "Q8JSpec",
-      value_vars_history = c("QA", "QB", "QC", "QD", "QE")) %>%
+      prob_var_item8 = "q8problem",
+      spec_var_item8 = "q8j_spec",
+      value_vars_history = c("qa", "qb", "qc",
+                             "qd", "qe")) %>%
     dplyr::select(7, 8, 15, 16, 17, 26
     ) %>%
     dplyr::collect()
